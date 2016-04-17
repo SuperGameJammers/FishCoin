53e3782bfa296632e9befa1da4df58bc1d6d2e2d
Zura Guerra <guerrazuramx@gmail.com>
2016-04-17 14:49:04 -0500

    Add styles

--- web/views/product_view.ex
+++ web/views/product_view.ex
@@ -1,3 +1,10 @@
 defmodule FishCoin.ProductView do
   use FishCoin.Web, :view
+
+  def discounted(price, discount) do
+    price = String.to_integer(price)
+    discount = String.to_integer(discount)
+    x = round(price/100) * discount
+    price - x
+  end
 end
