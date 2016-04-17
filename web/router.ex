c232c6b3554490c4169e9e5a6a21a12e7918008a
Zura Guerra <guerrazuramx@gmail.com>
2016-04-17 09:22:41 -0500

    Generate savings entity

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule FishCoin.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    
+    resources "/savings", SavingController
   end
 
   # Other scopes may use custom stacks.
